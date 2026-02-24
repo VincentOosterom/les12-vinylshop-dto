78eadd05c8a5ec0da7a1eb16dc9d22e41fc155bc
VincentOosterom <vincent@oosteromstudio.nl>
2026-02-24 14:18:16 +0100

    first push

---
+++ src/main/java/nl/les12vinylshopdto/les12vinylshopdto/Les12VinylshopDtoApplication.java
@@ -0,0 +1,13 @@
+package nl.les12vinylshopdto.les12vinylshopdto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Les12VinylshopDtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Les12VinylshopDtoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/les12vinylshopdto/les12vinylshopdto/Les12VinylshopDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.les12vinylshopdto.les12vinylshopdto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Les12VinylshopDtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
