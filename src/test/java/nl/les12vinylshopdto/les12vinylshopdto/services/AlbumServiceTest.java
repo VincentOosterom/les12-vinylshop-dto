ff62f2062ca5aec99585d198518fbadb19ed5d3b
VincentOosterom <vincent@oosteromstudio.nl>
2026-03-20 17:41:26 +0100

    test

---
+++ src/test/java/nl/les12vinylshopdto/les12vinylshopdto/services/AlbumServiceTest.java
@@ -0,0 +1,119 @@
+package nl.les12vinylshopdto.les12vinylshopdto.services;
+
+import nl.les12vinylshopdto.les12vinylshopdto.dto.album.AlbumRequestDTO;
+import nl.les12vinylshopdto.les12vinylshopdto.dto.album.AlbumResponseDTO;
+import nl.les12vinylshopdto.les12vinylshopdto.entities.AlbumEntity;
+import nl.les12vinylshopdto.les12vinylshopdto.entities.ArtistEntity;
+import nl.les12vinylshopdto.les12vinylshopdto.mapper.AlbumDTOMapper;
+import nl.les12vinylshopdto.les12vinylshopdto.mapper.AlbumExtendedDTOMapper;
+import nl.les12vinylshopdto.les12vinylshopdto.repositories.AlbumRepository;
+import nl.les12vinylshopdto.les12vinylshopdto.repositories.ArtistRepository;
+import nl.les12vinylshopdto.les12vinylshopdto.repositories.GenreRepository;
+import nl.les12vinylshopdto.les12vinylshopdto.repositories.PublisherRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AlbumServiceTest {
+
+    @Mock
+    private AlbumRepository albumRepository;
+
+    @Mock
+    private ArtistRepository artistRepository;
+
+    @Mock
+    private AlbumDTOMapper albumDTOMapper;
+
+    @Mock
+    private AlbumExtendedDTOMapper albumExtendedDTOMapper;
+
+    @Mock
+    private PublisherRepository publisherRepository;
+
+    @Mock
+    private GenreRepository genreRepository;
+
+    @InjectMocks
+    private AlbumService albumService;
+
+    private AlbumEntity albumEntity;
+    private AlbumResponseDTO albumResponseDTO;
+    private AlbumRequestDTO albumRequestDTO;
+
+    @BeforeEach
+    void setUp() {
+        albumEntity = new AlbumEntity();
+        albumEntity.setId(1L);
+        albumEntity.setTitle("Dark in the Middle");
+        albumEntity.setReleaseYear(2020);
+        albumEntity.setArtists(new HashSet<>());
+        albumEntity.setStockItems(List.of());
+
+        albumResponseDTO = new AlbumResponseDTO();
+        albumResponseDTO.setTitle("Dark in the Middle");
+
+        albumRequestDTO = new AlbumRequestDTO();
+        albumResponseDTO.setReleaseYear(2020);
+        albumRequestDTO.setTitle("Dark in the Middle");
+        albumRequestDTO.setGenreId(1L);
+        albumRequestDTO.setPublisherId(1L);
+    }
+
+
+    @Test
+    void findAllAlbums() {
+
+        List<AlbumEntity> albumEntities = List.of(albumEntity);
+        List<AlbumResponseDTO> dtos = List.of(albumResponseDTO);
+
+        when(albumRepository.findAll()).thenReturn(albumEntities);
+        when(albumDTOMapper.mapToDto(anyList())).thenReturn(dtos);
+
+        List<AlbumResponseDTO> result = albumService.findAllAlbums();
+
+        assertThat(result).hasSize(1);
+        assertThat(result.getFirst().getTitle()).isEqualTo("Dark in the Middle");
+    }
+
+    @Test
+    void findAlbumById() {
+    }
+
+    @Test
+    void createAlbum() {
+    }
+
+    @Test
+    void updateAlbum() {
+    }
+
+    @Test
+    void deleteAlbum() {
+    }
+
+    @Test
+    void linkArtist() {
+    }
+
+    @Test
+    void unlinkArtist() {
+    }
+
+    @Test
+    void getAlbumsWithStock() {
+    }
+}
